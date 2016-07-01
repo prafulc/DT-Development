84bd575ddd8f630d16caf962beb15d4d6143309f
deligence <rajit@deligence.com>
2016-07-01 17:59:19 +0530

    ionic tabs and sidemenu demo

---
+++ ionic-todo-tabs/www/js/app.js
@@ -0,0 +1,69 @@
+var app = angular.module('ionic-todo-app', ['ionic', 'LocalStorageModule'])
+
+app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+      .setPrefix('ionic-todo-app');
+  });
+
+app.controller('main', function ($scope, $ionicModal, localStorageService) { 
+    //initialize the tasks scope with empty array
+    $scope.tasks = [
+      {'title':'node', 'content':'nodejs'}
+
+
+    ];
+
+    //initialize the task scope with empty object
+    $scope.task = {};
+
+    var taskData = 'task';
+
+
+    // //configure the ionic modal before use
+    // $ionicModal.fromTemplateUrl('new-task-modal.html', {
+    //     scope: $scope,
+    //     animation: 'slide-in-up'
+    // }).then(function (modal) {
+    //     $scope.modal = modal;
+    // });
+
+    // $scope.openTaskModal = function() {
+    //   $scope.modal.show();
+    // };
+    
+    // $scope.closeTaskModal = function() {
+    //   $scope.modal.hide();
+    // };
+
+    $scope.getTasks = function () {
+          //fetches task from local storage
+          if (localStorageService.get(taskData)) {
+              $scope.tasks = localStorageService.get(taskData);
+          } else {
+              $scope.tasks = [
+                 {title:"node", content:"nodejs"}
+              ]
+          }
+    }
+    // $scope.createTask = function () {
+    //       //creates a new task
+    //       $scope.tasks.push($scope.task);
+    //       localStorageService.set(taskData, $scope.tasks);
+    //       $scope.task = {};
+    //       //close new task modal
+    //       $scope.modal.hide();
+    // }
+    // $scope.removeTask = function (index) {
+    //       //removes a task
+    //       $scope.tasks.splice(index, 1);
+    //       localStorageService.set(taskData, $scope.tasks);
+    // }
+    // $scope.completeTask = function (index) { 
+    //      //updates a task as completed 
+    //      if (index !== -1) {
+    //       $scope.tasks[index].completed = true; 
+    //      } 
+
+    //       localStorageService.set(taskData, $scope.tasks); 
+    // }
+})
